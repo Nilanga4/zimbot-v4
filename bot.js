668650ad4dd7bf503316fb36cedd2174257dbc6a
Nilanga4 <114267227+Nilanga4@users.noreply.github.com>
2022-10-25 15:46:24 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['+94742132265',''] //owner number +27634090203
+global.wame = ['+94742132265'] 
+global.pemilik = ['+94742132265'] //owner number +27634090203
+global.premium = ['+94742132265'] //premium number +27634090203
+global.pengguna = 'Nilanga md' //username
+global.footer = 'Nilanga md'
+global.botnma = 'Nilanga md' //bot name
+global.botname = 'Nilanga md'
+global.ownernma = 'Nilanga' //owner name
+global.packname = 'Nilanga' //sticker package name
+global.author = 'by Nilsnga md' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
